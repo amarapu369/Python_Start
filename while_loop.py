bb220a2b9fd34a7dd628182144f963dc3dfc3476
amarapu369 <ramprasadamarapu@gmail.com>
2024-02-20 18:12:08 +0530

    changes in while loop un commented

--- while_loop.py
+++ while_loop.py
@@ -2,13 +2,12 @@
 # while loop is used for the run the program in loop for a time or for number of repetations
 # we can use the both while and for loop for same work
 # example printing hello world 10 time 
-'''
 i = 1
 while i <= 10 :
     # print("my name is RAM ", i , "total was done")
     print(f"{i}.my name is RAM ")
     i = i + 1
-'''
+
 # example 1
 # sum of the 1 to 10 natural numbers 
 
