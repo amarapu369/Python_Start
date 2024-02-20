732a76ce6b8ad1904649ce7986fe090ffdcb1b0f
amarapu369 <ramprasadamarapu@gmail.com>
2024-02-20 17:47:31 +0530

    learning python

---
+++ string_indexing.py
@@ -0,0 +1,21 @@
+# string indexing
+lang = "Ram prasad"
+
+print(lang[0])
+print(len(lang))
+print(lang.lower())
+print(lang.upper())
+print(lang.title())
+print(lang.count("r"))
+print(lang[-1])
+# string slicing
+# print(lang[start argu :end argu-1])
+# print(lang[1:3])
+# print(lang[-1:-5])
+# print(lang[1:])
+# STEP ARGUMENT 
+# print('Ramprasad'[:5:-1])
+# print(lang[1:6:2])
+# print(lang[5::-1])
+# # string reversal
+print("ramprasad"[::-1])
