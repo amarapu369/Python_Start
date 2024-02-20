e763a952299226f8b6a0a4c8e9288afa73b2c047
amarapu369 <ramprasadamarapu@gmail.com>
2024-02-20 14:48:12 +0530

    Harsisth vasist videos

---
+++ keywords.py
@@ -0,0 +1,15 @@
+# in keyword uses in python 
+name = "ramprasad"
+# if "a" in "ramprasad":
+if "r" in name :
+    print("yes letter is present")
+else:
+    print("not present")
+# checking empty or not 
+# name = 'ramprasad'
+name  = input("enter yor name: \n")
+# name = ""
+if name :
+    print("string is not empty ")
+else:
+    print("string is empty")
