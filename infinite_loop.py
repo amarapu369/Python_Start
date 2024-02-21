5aa0225ae99dcff1eadc5ec9f03f7389255ac070
amarapu369 <ramprasadamarapu@gmail.com>
2024-02-21 14:59:23 +0530

    changes in infinite loop

---
+++ infinite_loop.py
@@ -0,0 +1,17 @@
+i =0 
+while i <= 10 :
+    print("my name is ramprasad")
+
+# these changes are made on 21-020-2024
+# above loop is the infinite loop because of the i value is not incerementing 
+
+# we must incerement the i value othgewise it will be the infinite loop 
+i =0 
+while i <= 10 :
+    print("my name is ramprasad")
+    i += 2
+
+# to run the  infinite loop intentionally 
+    
+    while True:
+        print("my name is Ramprasad")
