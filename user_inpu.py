df6e494940e9d809e4ebf07b1c7a265475aff8d3
amarapu369 <ramprasadamarapu@gmail.com>
2024-02-21 15:27:28 +0530

    new

---
+++ user_inpu.py
@@ -0,0 +1,13 @@
+# user input 
+# we will use the input function 
+# name = input('type your name\n')
+# print("hello " +name)
+# age = input("enter your age \n" )
+# print('your age is ' +age)
+
+name , age = input("enter your name and age \n").split()
+print(name)
+print(age)
+a = input("enter the first number\n")
+b = input("enter the second number\n" )
+c = input ("enter the thired number\n")
